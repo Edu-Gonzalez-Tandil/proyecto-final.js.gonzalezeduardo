28536ed02ea442473e9d3b7effea2c80e74cb92b
Edu Gonzalez <e.gonzalez1@outlook.es>
2022-07-12 12:57:59 -0300

    agrego array

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-let nombre = prompt("Ingrese su nombre: ");
-let apellido = prompt("Ingrese su apellido: ");
-let jugador = prompt("Ingrese si es jugador SI/NO");
-let edad = parseInt(prompt("Ingrese su edad. "));
 let cuota_activo = 4950;
 let cuota_cadete = 3100;
 let cuota_menores = 2700;
@@ -9,8 +5,35 @@ let cuota_dep_menores = 650;
 let cuota_dep_cadetes = 850;
 let costo_tercer_tiempo = 2000;
 let soc_activo = "SOCIO ACTIVO";
-let soc_cadete = "SOCIO CADETE MAYOR"
-let soc_menor = "SOCIO CADETE MENOR"
+let soc_cadete = "SOCIO CADETE MAYOR";
+let soc_menor = "SOCIO CADETE MENOR";
+
+
+class Socio {
+    constructor(nombre, apellido, jugador, edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.jugador = jugador;
+        this.edad = edad;
+    }
+}
+
+let lista_socio = [];
+
+for (let i = 0; i < 3; i++) {
+
+    let nombre = prompt("Ingrese su nombre:");
+    let apellido = prompt("Ingrese su apellido:");
+    let jugador = prompt("Ingrese SI/NO: ");
+    let edad = prompt("Ingrese su edad:");
+
+    let nuevo_socio = new Socio(nombre, apellido, jugador, edad);
+
+    lista_socio.push(nuevo_socio);
+
+}
+
+console.log(lista_socio);
 
 function categoria_social(edad) {
     
@@ -55,14 +78,21 @@ function costo_deportivo(edad) {
     }
 }
 
-console.log("Nombre: ", nombre);
-console.log("Apellido: ", apellido)
-console.log("Jugador: ", jugador);
-console.log("Edad: ", edad);
+for (let socio of lista_socio) {
 
-console.log("Categoria: ", categoria_social(edad));
-console.log("Cuota social: ", costo_social(edad));
-console.log("Cuota deportiva: " , costo_deportivo(edad));
-console.log("Costo total: " , costo_social(edad) + costo_deportivo(edad));
-console.log("Costo tercer tiempo (Mensual): " , tercer_tiempo(jugador == 'SI'));
+    console.log("Nombre:", socio.nombre);
+    console.log("Apellido:", socio.apellido);
+    console.log("Jugador:", socio.jugador)
+    console.log("Edad: ", socio.edad);
+    console.log("Categoria: ", categoria_social(socio.edad));
+    console.log("Cuota social: ", costo_social(socio.edad));
+    console.log("Cuota deportiva: " , costo_deportivo(socio.edad));
+    console.log("Costo total: " , costo_social(socio.edad) + costo_deportivo(socio.edad));
+    console.log(<--------------------------------------->);
+    
+
+}
+
+
+
 
